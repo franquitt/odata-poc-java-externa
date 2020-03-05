de611e1b080b2456aec0e496f17bc2d379d36e37
Franco Agresta Gonzalez <franquitt@gmail.com>
2020-03-05 12:58:03 -0300

    agregados datos de ejemplo

--- backendOdata/index.js
+++ backendOdata/index.js
@@ -3,17 +3,50 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 var ObjectId = Schema.Types.ObjectId;
 
-var server = odata('mongodb://mongo-db:27017/odata');
-server.resource('Address', {
+let addrSchema = {
   Street: String,
   City: String,
   ZipCode: String,
   Country: String
+};
+
+let carSchema = {
+  Identificador: Number,
+  Model: String,
+  ModelYear: String,
+  Price: Number,
+  Currency: String,
+};
+
+mongoose.connect('mongodb://mongo-db:27017/odata');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+	console.log("CONECTADO A LA DB! GENERANDO OBJETOS DE EJEMPLO");
+	var AddrModel = mongoose.model('Address', new Schema(addrSchema));
+	var CarModel = mongoose.model('cars', new Schema(carSchema));
+	
+	/*const objAddr = new AddrModel({
+	  Street: "Av Colon",
+	  City: "Cordoba",
+	  ZipCode: "5003",
+	  Country: "AR"
+	});*/
+	const objCar = new CarModel({
+		Identificador: 1,
+		Model: "Sedan",
+		ModelYear: "2014",
+		Price: 150000,
+		Currency: "ARS"
+	});
+	objCar.save(function (err, auto) {
+		if (err) return console.error(err);
+		console.log(auto);
+		var server = odata('mongodb://mongo-db:27017/odata');
+		server.resource('Address', addrSchema);
+		server.resource('cars', carSchema);
+		server.listen(3005);
+		console.log("Corriendo en http://localhost:3005");
+    });
+
 });
-server.resource('Cars', {
-  Name: String,
-  Description: String,
-  Address: {type: ObjectId, ref: "Address"},
-});
-server.listen(3005);
-console.log("Corriendo en http://localhost:3005");
